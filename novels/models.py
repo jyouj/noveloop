513cde6e939b599e8f997515e515b5849454b447
jyouj <youj.whenever.prime3@gmail.com>
2019-01-06 14:16:26 +0900

    Chap.2-1

--- novels/models.py
+++ novels/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Novel(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
